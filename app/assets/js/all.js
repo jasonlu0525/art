d031ca41912f2d67cb6dfefb175fa590f3b8c064
Jason <s099113302@gmail.com>
2021-07-11 17:13:53 +0800

    remove console.log

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,8 +3,6 @@
   let header_cart_list = document.querySelector('#header-cart-list');
   let cart = document.querySelector('#cart');
   let cart_CoverBg = document.querySelector('.cart-cover-bg');
-  console.log(header_cart_list);
-  console.log(cart);
   cart.addEventListener('click', function () {
 
     header_cart_list.classList.toggle('d-block');
