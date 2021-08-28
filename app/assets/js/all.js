04d2fbd94b24e92a79d1d62e6af45e638d1fa537
Jason <s099113302@gmail.com>
2021-08-28 15:38:25 +0800

    修改版

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,10 +3,37 @@
   let header_cart_list = document.querySelector('#header-cart-list');
   let cart = document.querySelector('#cart');
   let cart_CoverBg = document.querySelector('.cart-cover-bg');
+  const swiper = new Swiper('.guesee', {
+ 
+    // Responsive breakpoints
+    breakpoints: {
+   
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      768: {
+        slidesPerView:2,
+        spaceBetween: 18
+      },
+      992: {
+        slidesPerView:3,
+        spaceBetween: 30
+      },
+   
+    
+    }
+  })
+  
+  
   cart.addEventListener('click', function () {
 
     header_cart_list.classList.toggle('d-block');
     cart_CoverBg.classList.toggle('d-block');
   });
+
+
+
+
 
 })();
