8944edaeccd8658450226e1301aab5d097061db1
Jason <s099113302@gmail.com>
2021-08-28 15:41:18 +0800

    Update 2021-08-28T07:41:09.320Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -6,6 +6,23 @@
   var header_cart_list = document.querySelector('#header-cart-list');
   var cart = document.querySelector('#cart');
   var cart_CoverBg = document.querySelector('.cart-cover-bg');
+  var swiper = new Swiper('.guesee', {
+    // Responsive breakpoints
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 0
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 18
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      }
+    }
+  });
   cart.addEventListener('click', function () {
     header_cart_list.classList.toggle('d-block');
     cart_CoverBg.classList.toggle('d-block');
